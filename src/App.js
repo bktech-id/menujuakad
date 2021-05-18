0fbe22dedc9cbb174629deaf1d0019024ad00996
Ari Angga Nugraha <ari.angga@ui.ac.id>
2021-05-18 21:33:43 +0700

    test change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Menuju Akad
+          Menuju Akad Website
         </p>
         <a
           className="App-link"
