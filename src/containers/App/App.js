0474c9558d77c1bb9da440647b2b07af201a30c1
Ari Angga Nugraha <ari.angga@ui.ac.id>
2021-05-19 13:06:25 +0700

    re structure project

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Ainul Aulia Menuju Akad
+        </p>
+        </header>
+    </div>
+  );
+}
+
+export default App;
