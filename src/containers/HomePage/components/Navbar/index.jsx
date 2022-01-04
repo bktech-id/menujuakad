c671d3eebc8291a414c2e6f45d95688a62ad4c36
Ari Nugraha <nugrahaa878@gmail.com>
2022-01-04 10:18:01 +0700

    feat: add navbar for mobile

--- src/containers/HomePage/components/Navbar/index.jsx
+++ src/containers/HomePage/components/Navbar/index.jsx
@@ -8,11 +8,12 @@ import close from '../../../../assets/icon/close.png';
 const Navbar = () => {
   return (
     <nav>
-      <h2>Menuju Akad</h2>
-      <label for='check'>
-        <img className={styles.humberger} src={humberger} id='btn' />
-        <img className={styles.close} src={close} id='close' />
+      <input type='checkbox' id={styles.check} />
+      <label for={styles.check}>
+        <img className={styles.humberger} src={humberger} id={styles.btn} />
+        <img className={styles.close} src={close} id={styles.close} />
       </label>
+      <h2>Menuju Akad</h2>
       <ul>
         <li><a href='#'>Home</a></li>
         <li><a href='#'>Fitur</a></li>
