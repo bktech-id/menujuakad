e7362371e2e595d6e655cf1a608b9a7b3ba4d006
Ari Nugraha <nugrahaa878@gmail.com>
2022-01-04 11:57:41 +0700

    feat: add about section desktop

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
+import mainDesign from '../../assets/images/mainDesign.png';
 import Navbar from './components/Navbar';
 
 const HomePage = () => {
   return (
     <div>
       <Navbar className={styles.navbar} />
-       
-      <div className={styles.content}>
 
+      <div className={styles.about}>
+        <div className={styles.aboutWording}>
+          <h1>BUAT MOMEN SPESIALMU <br /> DIKETAHUI SEMUA <br /> ORANG</h1>
+          <p>Buat dan bagikan undangan Pernikahanmu. <br />
+            Pilih template yang sesuai dan keinginanmu lalu<br />
+            sebar ke siapapun yang kamu inginkan
+          </p>
+        </div>
+        <img className={styles.designDesktop} src={mainDesign} />
       </div>
     </div>
   )
