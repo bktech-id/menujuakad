4144376b11fef1011a0d0fd648ab0e42e56c4771
Ari Nugraha <nugrahaa878@gmail.com>
2022-01-04 09:12:52 +0700

    feat: add navbar

--- src/containers/HomePage/components/Navbar/index.jsx
+++ src/containers/HomePage/components/Navbar/index.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
+import humberger from '../../../../assets/icon/humberger.png';
+import close from '../../../../assets/icon/close.png';
+
 const Navbar = () => {
   return (
     <nav>
       <h2>Menuju Akad</h2>
+      <label for='check'>
+        <img className={styles.humberger} src={humberger} id='btn' />
+        <img className={styles.close} src={close} id='close' />
+      </label>
       <ul>
         <li><a href='#'>Home</a></li>
         <li><a href='#'>Fitur</a></li>
